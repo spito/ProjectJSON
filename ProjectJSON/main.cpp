79b2e58d0d0b8ed50d2e224cdfc0dc4c98afe27a
spito <jiri.weiser@gmail.com>
2014-10-10 13:40:39 +0200

    test: Add "test" for whole json parsing.

--- ProjectJSON/main.cpp
+++ ProjectJSON/main.cpp
@@ -2,11 +2,22 @@
 #include <string>
 
 #include "tests.h"
+#include "json/json.h"
 
 int main( int argc, char **argv ) {
 
 
     test::Test().tokenizer();
+
+
+    json::Factory factory( std::cerr );
+
+    auto handle = factory.parseFile( "test1.json" );
+
+    if ( !handle )
+        return -1;
+
+    std::cout << handle->toString() << std::endl;
 
     return 0;
 }
