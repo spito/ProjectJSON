6523088a722c91e1955ce3ea1c827ac4701c86a2
spito <jiri.weiser@gmail.com>
2014-10-25 14:16:26 +0200

    main: Improve exception safety.

--- ProjectJSON/main.cpp
+++ ProjectJSON/main.cpp
@@ -3,21 +3,24 @@
 #include "json/json.h"
 
 int main( int argc, char **argv ) {
+    try {
+        if ( argc < 2 ) {
+            std::cout << "Nothing to parse." << std::endl;
+            return 0;
+        }
 
-    if ( argc < 2 ) {
-        std::cout << "Nothing to parse." << std::endl;
-        return 0;
+        json::Factory factory( std::cerr );
+
+        auto handle = factory.parseFile( argv[ 1 ] );
+
+        if ( !handle || handle->isNull() )
+            return -1;
+
+        std::cout << "Dump of the JSON structure: " << std::endl;
+        std::cout << handle << std::endl;
     }
-
-    json::Factory factory( std::cerr );
-
-    auto handle = factory.parseFile( argv[ 1 ] );
-
-    if ( !handle || handle->isNull() )
-        return -1;
-
-    std::cout << "Dump of the JSON structure: " << std::endl;
-    std::cout << handle << std::endl;
-    
+    catch ( std::bad_alloc & ) {
+        std::cerr << "Allocation has failed!" << std::endl;
+    }
     return 0;
 }
