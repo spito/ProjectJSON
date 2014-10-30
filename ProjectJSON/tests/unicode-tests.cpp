910bba54bbffe206f5b3fd221a11c4d6df7d49c6
spito <jiri.weiser@gmail.com>
2014-10-30 10:40:22 +0100

    test: Improve Unicode tests.

--- ProjectJSON/tests/unicode-tests.cpp
+++ ProjectJSON/tests/unicode-tests.cpp
@@ -3,34 +3,54 @@
 
 using uchar = unsigned char;
 
+using namespace json::Unicode;
+
 TEST_CASE( "encode character", "[unicode]" ) {
 
-    std::string expected8 = "\t";
-    CHECK( expected8 == json::Unicode::encode( "09" ) );
+    std::string expected = "\t";
+    CHECK( expected == encode( "09" ) );
 
-    expected8 = "\n";
-    CHECK( expected8 == json::Unicode::encode( "0A" ) );
+    expected = "\n";
+    CHECK( expected == encode( "0A" ) );
 
-    std::string expected16 = "\xC3\xBF";
-    CHECK( expected16 == json::Unicode::encode( "ff" ) );
+    expected = "\xC3\xBF";
+    CHECK( expected == encode( "ff" ) );
 
-    std::string expected24 = "\xEA\xAA\xAA";
-    CHECK( expected24 == json::Unicode::encode( "AAAA" ) );
+    expected = "\xEA\xAA\xAA";
+    CHECK( expected == encode( "AAAA" ) );
 
+    expected = "\xE1\x88\xB4";
+    CHECK( expected == encode( "1234" ) );
 }
 
+TEST_CASE( "encoding limit cases", "[unicode]" ) {
+    std::string expected = "\x7f";
+    CHECK( expected == encode( "7f" ) );
+
+    expected = "\xC2\x80";
+    CHECK( expected == encode( "80" ) );
+
+    expected = "\xDF\xBF";
+    CHECK( expected == encode( "07ff" ) );
+
+    expected = "\xE0\xA0\x80";
+    CHECK( expected == encode( "0800" ) );
+
+    expected = "\xEF\xBF\xBF";
+    CHECK( expected == encode( "ffff" ) );
+}
 
 TEST_CASE( "decode string", "[unicode]" ) {
     std::string given = "text ";
-    given += json::Unicode::encode( "ff" );
-    CHECK( "text \\u00FF" == json::Unicode::decode( given ) );
+    given += encode( "ff" );
+    CHECK( "text \\u00FF" == decode( given ) );
 
-    given = json::Unicode::encode( "1f" );
-    CHECK( "\\x1F" == json::Unicode::decode( given, true ) );
+    given = encode( "1f" );
+    CHECK( "\\x1F" == decode( given, true ) );
 
-    given = json::Unicode::encode( "aaaa" );
-    CHECK( "\\uAAAA" == json::Unicode::decode( given ) );
+    given = encode( "aaaa" );
+    CHECK( "\\uAAAA" == decode( given ) );
 
-    given = json::Unicode::encode( "0a" );
-    CHECK( "\\n" == json::Unicode::decode( given ) );
+    given = encode( "0a" );
+    CHECK( "\\n" == decode( given ) );
 }
