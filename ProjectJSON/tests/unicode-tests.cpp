fa31f94b48780283e2f0b33ffb4de3a9e51a9759
spito <jiri.weiser@gmail.com>
2014-10-31 00:05:51 +0100

    test: Update tests.

--- ProjectJSON/tests/unicode-tests.cpp
+++ ProjectJSON/tests/unicode-tests.cpp
@@ -5,52 +5,59 @@ using uchar = unsigned char;
 
 using namespace json::Unicode;
 
-TEST_CASE( "encode character", "[unicode]" ) {
+TEST_CASE( "getUnicode", "[unicode]" ) {
 
     std::string expected = "\t";
-    CHECK( expected == encode( "09" ) );
+    CHECK( expected == getUTF8( "09" ) );
 
     expected = "\n";
-    CHECK( expected == encode( "0A" ) );
+    CHECK( expected == getUTF8( "000A" ) );
 
     expected = "\xC3\xBF";
-    CHECK( expected == encode( "ff" ) );
+    CHECK( expected == getUTF8( "ff" ) );
 
     expected = "\xEA\xAA\xAA";
-    CHECK( expected == encode( "AAAA" ) );
+    CHECK( expected == getUTF8( "AAAA" ) );
 
     expected = "\xE1\x88\xB4";
-    CHECK( expected == encode( "1234" ) );
+    CHECK( expected == getUTF8( "1234" ) );
+
+    expected = "\xF0\x90\x90\xB7";
+    CHECK( expected == getUTF8( "D801DC37" ) );
 }
 
-TEST_CASE( "encoding limit cases", "[unicode]" ) {
+TEST_CASE( "getUnicode limit cases", "[unicode]" ) {
     std::string expected = "\x7f";
-    CHECK( expected == encode( "7f" ) );
+    CHECK( expected == getUTF8( "7f" ) );
 
     expected = "\xC2\x80";
-    CHECK( expected == encode( "80" ) );
+    CHECK( expected == getUTF8( "80" ) );
 
     expected = "\xDF\xBF";
-    CHECK( expected == encode( "07ff" ) );
+    CHECK( expected == getUTF8( "07ff" ) );
 
     expected = "\xE0\xA0\x80";
-    CHECK( expected == encode( "0800" ) );
+    CHECK( expected == getUTF8( "0800" ) );
 
     expected = "\xEF\xBF\xBF";
-    CHECK( expected == encode( "ffff" ) );
+    CHECK( expected == getUTF8( "ffff" ) );
 }
 
-TEST_CASE( "decode string", "[unicode]" ) {
+TEST_CASE( "toAscii string", "[unicode]" ) {
     std::string given = "text ";
-    given += encode( "ff" );
-    CHECK( "text \\u00FF" == decode( given ) );
+    given += getUTF8( "ff" );
+    CHECK( "text \\u00FF" == toAscii( given ) );
 
-    given = encode( "1f" );
-    CHECK( "\\x1F" == decode( given, true ) );
+    given = getUTF8( "1f" );
+    CHECK( "\\u001F" == toAscii( given ) );
 
-    given = encode( "aaaa" );
-    CHECK( "\\uAAAA" == decode( given ) );
+    given = getUTF8( "aaaa" );
+    CHECK( "\\uAAAA" == toAscii( given ) );
 
-    given = encode( "0a" );
-    CHECK( "\\n" == decode( given ) );
+    given = getUTF8( "0a" );
+    CHECK( "\\n" == toAscii( given ) );
+
+    given = getUTF8( "D801DC37" );
+    CHECK( "\\uD801\\uDC37" == toAscii( given ) );
+
 }
